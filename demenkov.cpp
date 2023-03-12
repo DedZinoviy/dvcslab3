dd7be373dc6844c319df1cba876250a821b80a82
Zinoviy Kuzmitch <dima-bolgov.02@mail.ru>
2023-03-12 19:58:16 +0300

    Revert "Merge remote-tracking branch 'dimon/demenkov'"
    aboba
    
    This reverts commit 577928b61b79b01a2ca5895d54907d08ddd5c88b, reversing
    changes made to 6fb0f1f63a4bc6dbb6e7c9aebb0cff750a66b465.

--- demenkov.cpp
+++ demenkov.cpp
@@ -33,8 +33,6 @@ short *TruthTableSystem::getTruthTable(QStringList &subExpressions, int &rowAmou
     rowAmount = pow(2, variablesAmount);
     columnAmount = nodes.length();
 
-    int a = 2 + 2;
-
     return makeTruthTable();
 }
 
